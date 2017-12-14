3895daa1f41722999531e4b733abd83e0e0cfc1c
Raul R <raul.ramirez@garden.com.py>
2017-12-14 18:02:17 -0300

    Se corrige meta description de promociones

--- controllers/promociones.php
+++ controllers/promociones.php
@@ -10,6 +10,7 @@ class Promociones extends Controller {
         $this->view->promociones = $this->model->promociones();
         $this->view->title = SITE_TITLE . 'Promociones';
         $this->view->meta_sitename = META_SITENAME;
+        $this->view->meta_description = 'Lea más acerca de las promociones, ofertas especiales y precios con descuento de Kia aquí. Encuentre los mejores tratos y obtenga más en su siguiente reserva.';
         $this->view->render('header');
         $this->view->render('promociones/index');
         $this->view->render('footer');
